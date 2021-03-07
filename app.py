74b1660d1a1df30a9c51fecaabe8fe5882510841
Radu Ciobanu <rciobanu@d2p9kan0nxscnn.cloudfront.net>
2021-03-07 14:50:24 +0200

    updates

--- app.py
+++ app.py
@@ -2,25 +2,12 @@ from flask import Flask, render_template, url_for, request
 from time import ctime
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
-
-
-def Mifflin(mass, height, age, sex):
-    if sex == 'male':
-        s = 5
-    else:
-        s = -151
-    return(((10*float(mass))+(6.25*int(height))-(5*int(age)))+s)
-
-
-def Katch(mass, body_fat):
-    lbm = (float(mass)-(float(mass)*(int(body_fat)*0.01)))
-    return (370 + 21.6*lbm)
-
+from functions import Mifflin,Katch,your_tdee,all_tdees
+#source env/bin/activate
 
 app = Flask(__name__)
 Bootstrap(app)
 fa = FontAwesome(app)
-
 
 @app.route('/')
 def index():
@@ -34,14 +21,15 @@ def form_post():
     age = request.form['age']
     sex = request.form['sex']
     body_fat = request.form['body_fat']
-
+    activity_lvl = request.form.get('activity_lvl',2)
+    all_tdees=[]
     if body_fat == '':
-        my_result = (Mifflin(mass, height, age, sex))
-        return render_template('index.html', result=my_result)
+        bmr = int((Mifflin(mass, height, age, sex)))
     else:
-        my_result = (Katch(mass, body_fat))
-        return render_template('index.html', result=my_result)
-
-
-if __name__ == "__main__":
+        bmr = int((Katch(mass, body_fat)))
+    result=your_tdee(bmr,int(activity_lvl))
+    
+    return render_template('index.html', bmr=bmr,result=result,)
+        
+if __name__ == "__main__": 
     app.run(debug=True)
---
+++ index.py
@@ -0,0 +1,7 @@
+class index:
+    def __init__(self,mass,height,age,sex,body_fat):
+        self.mass=mass
+        self.height=height
+        self.age=age
+        self.sex=sex 
+        self.body_fat=body_fat
